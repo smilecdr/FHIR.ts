744677b1196ae56f1e8c069c9f9ff4686cb99ca3
AD1306 <adityadave.dave89@gmail.com>
2022-01-22 03:41:50 -0500

    added slot for R3

--- src/FHIR-R3.ts
+++ src/FHIR-R3.ts
@@ -1137,5 +1137,18 @@ export class Schedule extends Resource {
   comment: string;
 }
 
+export class slot extends Resource {
+  identifier: Identifier[];
+  serviceCategory: CodeableConcept;
+  serviceType: CodeableConcept[];
+  speciality: CodeableConcept[];
+  appointmentType: CodeableConcept;
+  schedule: Reference;
+  status: string;
+  start: string;
+  end: string;
+  overbooked: boolean;
+  comment: string;
+}
 
 
