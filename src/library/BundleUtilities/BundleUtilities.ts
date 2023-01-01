2a97b1aa7ae6a509eb3fa98c82d5c14d2f9872a9
ad1306 <adave.dev@gmail.com>
2023-01-01 12:07:55 -0500

    add bundle filters for BundleUtilities

--- src/library/BundleUtilities/BundleUtilities.ts
+++ src/library/BundleUtilities/BundleUtilities.ts
@@ -1,3 +1,12 @@
 export class BundleUtilities {
 
+    /**
+     * 
+     * @param bundleEntry Bundle.entry[] i.e. the bundle entries to filter
+     * @param resourceTypeToFilter ResourceType to filter from bundle entries
+     * @returns array of resources
+     */
+    static getResourcesFromBundle(bundleEntry: any[], resourceTypeToFilter: string): any[] {
+        return bundleEntry?.length ? bundleEntry.filter(x => x['resource']['resourceType'] === resourceTypeToFilter) : [];
+    }
 }
--- src/library/TypeUtilities/TypeUtilities.ts
+++ src/library/TypeUtilities/TypeUtilities.ts
@@ -3,28 +3,6 @@ import { ValidationUtilities } from "../ValidationUtilities/ValidationUtilities"
 export class TypeUtilities {
   /**
    * 
-   * @param arrayToMatchValueIn - Array of JSON objects in which we want to find values
-   * @param valueToMatch - Value we want to find in @param arrayToMatchValueIn
-   * @returns array with results based on matches found
-   */
-  static findMatchingValuesInArray(arrayToMatchValueIn: Array<object>, valueToMatch: object): Array<object> {
-    let matchingResults: Array<object> = [];
-    const valueToMatchIsValidJson = ValidationUtilities.isValidJson(valueToMatch);
-    const valueToMatchInArrayIncludesValidJson = arrayToMatchValueIn.length > 0 ? ValidationUtilities.isValidJson(arrayToMatchValueIn[0]) : false;
-    if (valueToMatchIsValidJson && valueToMatchInArrayIncludesValidJson) {
-      for (let index = 0; index < arrayToMatchValueIn.length; index++) {
-        const matchAgainstStringified = JSON.stringify(arrayToMatchValueIn[index]);
-        const matchValueStringified = JSON.stringify(valueToMatch);
-        if (matchAgainstStringified === matchValueStringified) {
-          matchingResults.push(arrayToMatchValueIn[index]);
-        }
-      }
-    }
-    return matchingResults;
-  }
-
-  /**
-   * 
    * @param identifierList list of identifiers
    * @param propertyToCompare identifier property to compare
    * @param propertyValue value we want to compare against
