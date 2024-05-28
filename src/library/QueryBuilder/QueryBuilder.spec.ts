82b0688cd5a28301ccf572774aab7737b20fe109
ad1306 <adave.dev@gmail.com>
2024-05-27 21:41:58 -0400

    wrap up sort functionality

--- src/library/QueryBuilder/QueryBuilder.spec.ts
+++ src/library/QueryBuilder/QueryBuilder.spec.ts
@@ -1,8 +1,9 @@
+import { SORT_ORDER } from "../constants";
 import { QueryBuilder } from "./QueryBuilder";
 
 describe("QueryBuilder", () => {
 
-  let queryBuilder;
+  let queryBuilder: QueryBuilder;
 
   beforeEach(() => {
     queryBuilder = new QueryBuilder();
@@ -17,7 +18,7 @@ describe("QueryBuilder", () => {
     expect(queryBuilder.getBaseResource()).toEqual(expected);
   })
 
-  it('should make query for _include', () => {
+  it('include() should make query for _include', () => {
     // setup
     const expected: String = "Patient?_include=Patient:organization"
     // execute
@@ -26,43 +27,50 @@ describe("QueryBuilder", () => {
     expect(queryBuilder.getCompleteUrl()).toEqual(expected);
   });
 
-  it('should make query for multiple _include', () => {
+  it('include() should make query for multiple _include', () => {
     // setup
     const expected: String = "Patient?_include=Patient:organization&_include=Patient:link"
     // execute
-    queryBuilder.setBaseResource("Patient").include("organization").include("link");
+    queryBuilder.setBaseResource("Patient")
+                .include("organization")
+                .include("link");
     // validate
     expect(queryBuilder.getCompleteUrl()).toEqual(expected);
   });
 
-  it('should make query for _revinclude', () => {
+  it('revinclude() should make query for _revinclude', () => {
     // setup
     const expected: String = "MedicationRequest?_revinclude=CarePlan:activity-reference"
     // execute
-    queryBuilder.setBaseResource("MedicationRequest").revinclude("CarePlan", "activity-reference");
+    queryBuilder.setBaseResource("MedicationRequest")
+                .revinclude("CarePlan", "activity-reference");
     // validate
     expect(queryBuilder.getCompleteUrl()).toEqual(expected);
   });
 
-  it('should make query for multiple _revinclude', () => {
+  it('revinclude() should make query for multiple _revinclude', () => {
     // setup
     const expected: String = "MedicationRequest?_revinclude=CarePlan:activity-reference&_revinclude=Observation:based-on"
     // execute
-    queryBuilder.setBaseResource("MedicationRequest").revinclude("CarePlan", "activity-reference").revinclude("Observation", "based-on");
+    queryBuilder.setBaseResource("MedicationRequest")
+                .revinclude("CarePlan", "activity-reference")
+                .revinclude("Observation", "based-on");
     // validate
     expect(queryBuilder.getCompleteUrl()).toEqual(expected);
   });
 
-  it('should make query for _include & _revinclude', () => {
+  it('include() and revinclude() should make query for _include & _revinclude', () => {
     // setup
     const expected: String = "MedicationRequest?_revinclude=CarePlan:activity-reference&_include=MedicationRequest:encounter"
     // execute
-    queryBuilder.setBaseResource("MedicationRequest").revinclude("CarePlan", "activity-reference").include("encounter");
+    queryBuilder.setBaseResource("MedicationRequest")
+                .revinclude("CarePlan", "activity-reference")
+                .include("encounter");
     // validate
     expect(queryBuilder.getCompleteUrl()).toEqual(expected);
   });
 
-  it('should make query for _include all', () => {
+  it('includeAll() should make query for _include all', () => {
     // setup
     const expected: String = "MedicationRequest?_include=*"
     // execute
@@ -71,7 +79,7 @@ describe("QueryBuilder", () => {
     expect(queryBuilder.getCompleteUrl()).toEqual(expected);
   });
 
-  it('should make query for _revinclude all', () => {
+  it('revincludeAll() should make query for _revinclude all', () => {
     // setup
     const expected: String = "MedicationRequest?_revinclude=*"
     // execute
@@ -80,12 +88,50 @@ describe("QueryBuilder", () => {
     expect(queryBuilder.getCompleteUrl()).toEqual(expected);
   });
 
-  it('should make query for _include all & _revinclude all', () => {
+  it('includeAll() & revincludeAll() should make query for _include all & _revinclude all', () => {
     // setup
     const expected: String = "MedicationRequest?_revinclude=*&_include=*"
     // execute
-    queryBuilder.setBaseResource("MedicationRequest").revincludeAll().includeAll();
+    queryBuilder.setBaseResource("MedicationRequest")
+                .revincludeAll()
+                .includeAll();
     // validate
     expect(queryBuilder.getCompleteUrl()).toEqual(expected);
+  });
+
+  it('sort() should make query for _sort', () => {
+    // setup
+    const expected: String = "Observation?_sort=status,-date"
+    // execute
+    queryBuilder.setBaseResource("Observation")
+                .sort("status", SORT_ORDER.ASCENDING)
+                .sort("date", SORT_ORDER.DESCENDING);
+    // validate
+    expect(queryBuilder.getCompleteUrl()).toEqual(expected);
+  });
+
+  it('sort(), include() and revincludeAll() should make query for _sort, _include & _revinclude', () => {
+    // setup
+    const expected: String = "Observation?_revinclude=*&_include=Observation:based-on&_sort=status"
+    // execute
+    queryBuilder.setBaseResource("Observation")
+                .revincludeAll()
+                .include("based-on")
+                .sort("status", SORT_ORDER.ASCENDING);
+    // validate
+    expect(queryBuilder.getCompleteUrl()).toEqual(expected);
+  });
+
+  it('resetQuery() should reset query', () => {
+    // setup
+    const expected: String = ""
+    queryBuilder.setBaseResource("Observation")
+                .revincludeAll()
+                .include("based-on")
+                .sort("status", SORT_ORDER.ASCENDING);
+    // execute
+    queryBuilder.resetQuery();
+    // validate
+    expect(queryBuilder.getBaseResource()).toEqual(expected);
   });
 });
--- src/library/QueryBuilder/QueryBuilder.ts
+++ src/library/QueryBuilder/QueryBuilder.ts
@@ -1,14 +1,18 @@
+import { SORT_ORDER } from "../constants";
+
 export class QueryBuilder {
 
   readonly REV_INCLUDE_KEYWORD = "_revinclude";
   readonly INCLUDE_KEYWORD = "_include";
+  readonly SORT_KEYWORD = "_sort";
   readonly WILDCARD_ASTERIK = "*";
   readonly EQUALS = "=";
   readonly COLON = ":";
   readonly QUERY_DELIMETER = "&";
-  // rename to QueryBuilder
+  readonly COMMA = ",";
   private baseResource: String = "";
   private singularQueries: String[] = [];
+  private sortQueries: String[] =  [];
 
   /**
    * @returns base resource for the query
@@ -20,6 +24,7 @@ export class QueryBuilder {
   /**
    * @param resourceType Base ResourceType for the query 
    * sets base resource type for the query
+   * i.e. which resource the query will performed for
    */
   setBaseResource(resourceType: String) {
     this.baseResource = resourceType;
@@ -27,24 +32,24 @@ export class QueryBuilder {
   }
 
   /**
-   * @param reference SearchParameter reference to targetted resource
+   * @param searchParameter SearchParameter reference to targeted resource
    */
-  include(reference: String) {
-    this.singularQueries.push(this.INCLUDE_KEYWORD + this.EQUALS + this.baseResource + this.COLON + reference);
+  include(searchParameter: String) {
+    this.singularQueries.push(this.INCLUDE_KEYWORD + this.EQUALS + this.baseResource + this.COLON + searchParameter);
     return this;
   }
 
   /**
-   * @param resourceType Target ResourceType
-   * @param reference SearchParameter refernce from target resource to baseResource
+   * @param resourceType Source ResourceType 
+   * @param searchParameter SearchParameter reference from target resource to baseResource
    */
-  revinclude(resourceType: String, reference: String) {
-    this.singularQueries.push(this.REV_INCLUDE_KEYWORD + this.EQUALS + resourceType + this.COLON + reference);
+  revinclude(resourceType: String, searchParameter: String) {
+    this.singularQueries.push(this.REV_INCLUDE_KEYWORD + this.EQUALS + resourceType + this.COLON + searchParameter);
     return this;
   }
 
   /**
-   * _include all references
+   * _include all references in query
    */
   includeAll() {
     this.singularQueries.push(this.INCLUDE_KEYWORD + this.EQUALS + this.WILDCARD_ASTERIK);
@@ -52,15 +57,55 @@ export class QueryBuilder {
   }
 
   /**
-   * _revinclude all references
+   * _revinclude all references in query
    */
   revincludeAll() {
     this.singularQueries.push(this.REV_INCLUDE_KEYWORD + this.EQUALS + this.WILDCARD_ASTERIK);
     return this;
   }
 
+  /**
+   * @param searchParameter search parameter for the element to sort on
+   * @param sortOrder ASCENDING or DESCENDING
+   */
+  sort(searchParameter: String, sortOrder: SORT_ORDER) {
+    if(sortOrder === SORT_ORDER.ASCENDING) {
+      this.sortQueries.push(searchParameter);
+    } else {
+      this.sortQueries.push("-" + searchParameter);
+    }
+    return this;
+  }
+
+  /**
+   * Resets queries to it empty state
+   */
+  resetQuery() {
+    this.sortQueries = [];
+    this.singularQueries = [];
+    this.baseResource = "";
+    return this;
+  }
+
+  /**
+   * @returns complete generated query 
+   */
   getCompleteUrl(): String {
-    return this.baseResource + "?" +  this.singularQueries.join(this.QUERY_DELIMETER);
+    let completeUrl = this.baseResource + "?";
+    if(this.singularQueries.length > 0) {
+      completeUrl +=  this.singularQueries.join(this.QUERY_DELIMETER);
+    }
+    completeUrl += this.createSortQuery();
+    return completeUrl; 
+  }
+  
+  private createSortQuery(): String {
+    let sortQuery = "";
+    const sortDelimeter = this.singularQueries.length > 0 ? this.QUERY_DELIMETER : "";
+    if(this.sortQueries.length > 0) {
+      sortQuery += sortDelimeter + this.SORT_KEYWORD + this.EQUALS + this.sortQueries.join(this.COMMA);
+    }
+    return sortQuery;
   }
   
 }
