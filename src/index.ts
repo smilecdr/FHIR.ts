16f55689e6d6b4c625114869c43a2b5f76859b35
ad1306 <adave.dev@gmail.com>
2024-05-31 15:38:50 -0400

    add definitions for r5

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as fhirR5 from './FHIR-R5/models';
 import * as fhirR4 from './FHIR-R4/classes/models-r4';
 import * as IfhirR4 from './FHIR-R4/interfaces/IModel';
 import * as fhirR3 from './FHIR-R3';
@@ -5,5 +6,4 @@ import * as dstu2 from './FHIR-DSTU2';
 import { ResourceUtils } from './library/ResourceUtils/ResourceUtils';
 import { BundleUtils } from './library/BundleUtils/BundleUtils';
 import { QueryBuilder } from './library/QueryBuilder/QueryBuilder';
-export { fhirR4, fhirR3, IfhirR4, dstu2, ResourceUtils, BundleUtils, QueryBuilder };
-
+export { fhirR5, fhirR4, fhirR3, IfhirR4, dstu2, ResourceUtils, BundleUtils, QueryBuilder };
