7dff3a5d712c47484456a268482419c554b6db54
taha.attari@smilecdr.com <taha.attari@smilecdr.com>
2024-05-31 13:11:06 -0400

    revert semicolons

--- src/library/ResourceUtils/ResourceUtils.ts
+++ src/library/ResourceUtils/ResourceUtils.ts
@@ -1,13 +1,13 @@
-import * as DSTU2 from "../../FHIR-DSTU2"
-import * as R3 from "../../FHIR-R3"
-import { Coding as r4Coding } from "../../FHIR-R4/classes/coding"
-import { Extension as r4Extension } from "../../FHIR-R4/classes/extension"
-import { Identifier as r4Identifier } from "../../FHIR-R4/classes/identifier"
-export type Identifier = r4Identifier & R3.Identifier & DSTU2.Identifier
-type IdentifierKeys = keyof Identifier
-export type Extension = r4Extension & R3.Extension & DSTU2.Extension
-export type Coding = r4Coding & R3.Coding & DSTU2.Coding
-type CodingKeys = keyof Coding
+import * as DSTU2 from "../../FHIR-DSTU2";
+import * as R3 from "../../FHIR-R3";
+import { Coding as r4Coding } from "../../FHIR-R4/classes/coding";
+import { Extension as r4Extension } from "../../FHIR-R4/classes/extension";
+import { Identifier as r4Identifier } from "../../FHIR-R4/classes/identifier";
+export type Identifier = r4Identifier & R3.Identifier & DSTU2.Identifier;
+type IdentifierKeys = keyof Identifier;
+export type Extension = r4Extension & R3.Extension & DSTU2.Extension;
+export type Coding = r4Coding & R3.Coding & DSTU2.Coding;
+type CodingKeys = keyof Coding;
 export class ResourceUtils {
   /**
    *
@@ -17,11 +17,11 @@ export class ResourceUtils {
    * @limitation currently just supports get for top level property on resource
    */
   getResourceProperty(inputJson: object, propertyName: string) {
-    let resourcePropertyValue = null
+    let resourcePropertyValue = null;
     if (inputJson.hasOwnProperty(propertyName)) {
-      resourcePropertyValue = inputJson[propertyName]
+      resourcePropertyValue = inputJson[propertyName];
     }
-    return resourcePropertyValue
+    return resourcePropertyValue;
   }
 
   /**
@@ -39,7 +39,7 @@ export class ResourceUtils {
   ): Identifier[] {
     return identifierList?.length
       ? identifierList.filter((x) => x[propertyToCompare] === propertyValue)
-      : []
+      : [];
   }
 
   /**
@@ -51,7 +51,7 @@ export class ResourceUtils {
   getExtensionsByUrl(extensionList: Extension[], extensionUrl: string): Extension[] {
     return extensionList?.length
       ? extensionList.filter((x) => x["url"] === extensionUrl)
-      : []
+      : [];
   }
 
   /**
@@ -68,7 +68,7 @@ export class ResourceUtils {
   ): Coding[] {
     return codingList?.length
       ? codingList.filter((x) => x[propertyToCompare] === propertyValue)
-      : []
+      : [];
   }
 
   /**
@@ -78,39 +78,39 @@ export class ResourceUtils {
    * @returns array of elements found at the provided path
    */
   getValuesAtResourcePath(resource: any, elementPath: string): any[] {
-    const pathSections = elementPath.split(".")
-    let resourcePathValue
+    const pathSections = elementPath.split(".");
+    let resourcePathValue;
     for (let index = 1; index < pathSections.length; index++) {
-      const subPaths = pathSections[index]
-      resourcePathValue = resourcePathValue ? resourcePathValue[subPaths] : resource[subPaths]
+      const subPaths = pathSections[index];
+      resourcePathValue = resourcePathValue ? resourcePathValue[subPaths] : resource[subPaths];
       if (resourcePathValue) {
         if (this.isPrimitive(resourcePathValue)) {
-          return [resourcePathValue]
+          return [resourcePathValue];
         } else if (Array.isArray(resourcePathValue) && resourcePathValue.length > 0) {
-          let resultSet = []
+          let resultSet = [];
           for (let subPathIndex = 0; subPathIndex < resourcePathValue.length; subPathIndex++) {
-            const subPathValue = resourcePathValue[subPathIndex]
+            const subPathValue = resourcePathValue[subPathIndex];
             if (this.isPrimitive(subPathValue)) {
-              resultSet.push(subPathValue)
+              resultSet.push(subPathValue);
             } else {
               resultSet.push(...this.getValuesAtResourcePath(subPathValue,
-                pathSections.slice(index).join(".")))
+                pathSections.slice(index).join(".")));
             }
           }
-          return resultSet
+          return resultSet;
         } else if (typeof (resourcePathValue) === 'object') {
           return this.getValuesAtResourcePath(resourcePathValue,
-            pathSections.slice(index).join("."))
+            pathSections.slice(index).join("."));
         }
       } else {
-        break
+        break;
       }
     }
-    return []
+    return [];
   }
 
   private isPrimitive(value: any) {
-    return typeof (value) === "boolean" || typeof (value) === "string"
+    return typeof (value) === "boolean" || typeof (value) === "string";
   }
 
   /**
@@ -118,21 +118,21 @@ export class ResourceUtils {
    * @returns array of references inside a resource
    */
   getAllReferencesFromResource(resource: any): string[] {
-    const stringifiedResource = JSON.stringify(resource)
-    const referenceJsonString = '"reference":'
-    let references = []
-    let cursor = stringifiedResource.indexOf(referenceJsonString, 0)
+    const stringifiedResource = JSON.stringify(resource);
+    const referenceJsonString = '"reference":';
+    let references = [];
+    let cursor = stringifiedResource.indexOf(referenceJsonString, 0);
     while (cursor > -1) {
-      const referenceStart = stringifiedResource.indexOf(referenceJsonString, cursor) + referenceJsonString.length
-      const referenceEnd = stringifiedResource.indexOf('"', referenceStart + 1)
-      const reference = stringifiedResource.substring(referenceStart, referenceEnd)
+      const referenceStart = stringifiedResource.indexOf(referenceJsonString, cursor) + referenceJsonString.length;
+      const referenceEnd = stringifiedResource.indexOf('"', referenceStart + 1);
+      const reference = stringifiedResource.substring(referenceStart, referenceEnd);
       // this means the reference ends started reading from start again
       if (referenceEnd < cursor) {
-        break
+        break;
       }
-      references.push(reference)
-      cursor = referenceEnd
+      references.push(reference);
+      cursor = referenceEnd;
     }
-    return references
+    return references;
   }
 }
