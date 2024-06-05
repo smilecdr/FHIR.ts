294361c085f376eee61d47ecd9b0dd58ca8de9fe
ad1306 <adave.dev@gmail.com>
2024-06-05 15:35:34 -0400

    add patch utils

---
+++ src/library/PatchUtils/PatchUtils.spec.ts
@@ -0,0 +1,138 @@
+import { PatchDatatype } from "../constants";
+import { PatchUtils } from "./PatchUtils";
+describe("PatchUtils", () => {
+
+  let patchUtils = new PatchUtils();
+
+  it('createReplaceParameters() should create Parameters for FHIR patch replace operation', () => {
+    // setup
+    const expected = {
+      "resourceType": "Parameters",
+      "parameter": [ {
+        "name": "operation",
+        "part": [ {
+          "name": "type",
+          "valueCode": "replace"
+        }, {
+          "name": "path",
+          "valueString": "Patient.birthDate"
+        }, {
+          "name": "value",
+          "valueDate": "1930-01-01"
+        } ]
+      } ]
+    };
+    // execute
+    const actual = patchUtils.createReplaceParameters("Patient.birthDate", "1930-01-01", PatchDatatype.DATE);
+    // validate
+    expect(actual).toEqual(expected);
+  });
+
+  it('createDeleteParameters() should create Parameters for FHIR patch delete operation', () => {
+    // setup
+    const expected = {
+      "resourceType": "Parameters",
+      "parameter": [ {
+        "name": "operation",
+        "part": [ {
+          "name": "type",
+          "valueCode": "delete"
+        }, {
+          "name": "path",
+          "valueString": "Patient.status"
+        }]
+      } ]
+    };
+    // execute
+    const actual = patchUtils.createDeleteParameters("Patient.status");
+    // validate
+    expect(actual).toEqual(expected);
+  });
+
+  it('createMoveParameters() should create Parameters for FHIR patch move operation', () => {
+    // setup
+    const expected = {
+      "resourceType": "Parameters",
+      "parameter": [ {
+        "name": "operation",
+        "part": [ {
+          "name": "type",
+          "valueCode": "move"
+        }, {
+          "name": "path",
+          "valueString": "Patient.identifier"
+        }, {
+          "name": "source",
+          "valueInteger": 0
+        }, {
+          "name": "destination",
+          "valueInteger": 1
+        }]
+      } ]
+    };
+    // execute
+    const actual = patchUtils.createMoveParameters("Patient.identifier", 0, 1);
+    // validate
+    expect(actual).toEqual(expected);
+  });
+
+  it('createAddParameters() should create Parameters for FHIR patch add operation', () => {
+    // setup
+    const expected = {
+      "resourceType": "Parameters",
+      "parameter": [ {
+        "name": "operation",
+        "part": [ {
+          "name": "type",
+          "valueCode": "add"
+        }, {
+          "name": "path",
+          "valueString": "Patient"
+        }, {
+          "name": "name",
+          "valueString": "birthDate"
+        }, {
+          "name": "value",
+          "valueDate": "1930-01-01"
+        }]
+      } ]
+    };
+    // execute
+    const actual = patchUtils.createAddParameters("Patient", "birthDate", "1930-01-01", PatchDatatype.DATE);
+    // validate
+    expect(actual).toEqual(expected);
+  });
+
+  it('createInsertParameters() should create Parameters for FHIR patch add operation', () => {
+    // setup
+    const identifier = {
+      "system": "http://some-system.com",
+      "value": "someValue"
+    }
+    const expected = {
+      "resourceType": "Parameters",
+      "parameter": [ {
+        "name": "operation",
+        "part": [ {
+          "name": "type",
+          "valueCode": "insert"
+        }, {
+          "name": "path",
+          "valueString": "Patient.identifier"
+        }, {
+          "name": "index",
+          "valueInteger": 1
+        }, {
+          "name": "value",
+          "valueIdentifier": identifier
+        }]
+      } ]
+    };
+    // execute
+    const actual = patchUtils.createInsertParameters("Patient.identifier", identifier, PatchDatatype.IDENTIFIER, 1);
+    // validate
+    expect(actual).toEqual(expected);
+  });
+  
+  
+});
---
+++ src/library/PatchUtils/PatchUtils.ts
@@ -0,0 +1,194 @@
+import { PatchDatatype } from "../constants";
+
+/**
+ * This a simple utility to create Parameters resource for FHIR patch operation
+ * https://www.hl7.org/fhir/fhirpatch.html
+ */
+export class PatchUtils {
+  private replaceOperation = "replace";
+  private deleteOperation = "delete";
+  private moveOperation = "move";
+  private addOperation = "add";
+  private insertOperation = "insert";
+
+  /**
+   *
+   * @param path the path to replace value of
+   * @param value the value to replace with
+   * @param valueDataType the data type of value
+   * @returns Parameters resource for FHIR patch replace operation
+   */
+  createReplaceParameters(
+    path: string,
+    value: any,
+    valueDataType: PatchDatatype
+  ) {
+    return {
+      resourceType: "Parameters",
+      parameter: [
+        {
+          name: "operation",
+          part: [
+            {
+              name: "type",
+              valueCode: this.replaceOperation,
+            },
+            {
+              name: "path",
+              valueString: path,
+            },
+            {
+              name: "value",
+              [valueDataType]: value,
+            },
+          ],
+        },
+      ],
+    };
+  }
+
+  /**
+   *
+   * @param path the path to delete value of
+   * @returns Parameters resource for FHIR patch delete operation
+   */
+  createDeleteParameters(path: string) {
+    return {
+      resourceType: "Parameters",
+      parameter: [
+        {
+          name: "operation",
+          part: [
+            {
+              name: "type",
+              valueCode: this.deleteOperation,
+            },
+            {
+              name: "path",
+              valueString: path,
+            },
+          ],
+        },
+      ],
+    };
+  }
+
+  /**
+   *
+   * @param path the path to property where the index of items needs to be changed
+   * @param source the index to move
+   * @param destination the index where the value will be moved
+   * @returns Parameters resource for FHIR patch move operation
+   */
+  createMoveParameters(path: string, source: number, destination: number) {
+    return {
+      resourceType: "Parameters",
+      parameter: [
+        {
+          name: "operation",
+          part: [
+            {
+              name: "type",
+              valueCode: this.moveOperation,
+            },
+            {
+              name: "path",
+              valueString: path,
+            },
+            {
+              name: "source",
+              valueInteger: source,
+            },
+            {
+              name: "destination",
+              valueInteger: destination,
+            },
+          ],
+        },
+      ],
+    };
+  }
+
+  /**
+   * @param path the path at which to add the content
+   * @param name name of the property to add
+   * @param value the value to replace with
+   * @param valueDataType the data type of value
+   * @returns Parameters resource for FHIR patch add operation
+   */
+  createAddParameters(
+    path: string,
+    name: string,
+    value: any,
+    valueDataType: PatchDatatype
+  ) {
+    return {
+      resourceType: "Parameters",
+      parameter: [
+        {
+          name: "operation",
+          part: [
+            {
+              name: "type",
+              valueCode: this.addOperation,
+            },
+            {
+              name: "path",
+              valueString: path,
+            },
+            {
+              name: "name",
+              valueString: name,
+            },
+            {
+              name: "value",
+              [valueDataType]: value,
+            },
+          ],
+        },
+      ],
+    };
+  }
+
+  /**
+   * 
+   * @param path the path at which to insert the value
+   * @param value the value to insert
+   * @param valueDataType the datatype of the value
+   * @param index the index at which the value should be inserted
+   * @returns Parameters resource for FHIR patch insert operation
+   */
+  createInsertParameters(
+    path: string,
+    value: any,
+    valueDataType: PatchDatatype,
+    index: number
+  ) {
+    return {
+      resourceType: "Parameters",
+      parameter: [
+        {
+          name: "operation",
+          part: [
+            {
+              name: "type",
+              valueCode: this.insertOperation,
+            },
+            {
+              name: "path",
+              valueString: path,
+            },
+            {
+              name: "index",
+              valueInteger: index,
+            },
+            {
+              name: "value",
+              [valueDataType]: value,
+            },
+          ],
+        },
+      ],
+    };
+  }
+}
